02b6cfebeb1df2512318c763bf42b0dbc5e203d6
Rodrigo Chaves <oxygeniumrodrigo@hotmail.com>
2022-09-28 00:04:26 -0400

    s

--- src/pages/Auction/index.tsx
+++ src/pages/Auction/index.tsx
@@ -47,7 +47,7 @@ export const Auction = () => {
 
   const auctionID = window.location.pathname.split('/').pop() as string;
   const websocket = new WebSocket(
-    `ws://144.202.15.60:8080/ws?auctionID=${auctionID}&userName=${userInfo.userLogged}`,
+    `wss://144.202.15.60:8080/wss?auctionID=${auctionID}&userName=${userInfo.userLogged}`,
   );
   const [auctionData, setAuctionData] = useState<RespAuctionType>();
 
